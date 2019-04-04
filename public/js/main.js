3fdffd61486b831fc16b3af798d86a1c494ecba8
unknown <dyogo.vianaa@gmail.com>
2019-04-04 02:05:52 -0300

    Implementar o campo de digitação; Add contadores de caracteres e palavras; Eventos com a função .on() do jQuery; Evento de click; Evento de input; Contagem de palavras com uma expressão regular.

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,26 @@
 // main.js
 
+// Modelo a ser escrito.
 var frase = $(".frase").text ();
 var numeroPalavras = frase.split (" ").length; 
 var tamanhoFrase = $("#tamanho-frase");
 
 tamanhoFrase.text (numeroPalavras);
+
+
+
+// Campo de digitação;
+var campo = $(".campo-digitacao");
+
+campo.on ("input", function () {
+    
+    var conteudo = campo.val (); // Ler os value's.
+
+    // Contador de palavras.
+    var quantidadePalavras = conteudo.split (/\S+/).length - 1; // Exp. regular p/ n contar os espaços.
+    $("#contador-palavras").text (quantidadePalavras);
+
+    // Contador de caracteres.
+    var quantidadeCaracteres = conteudo.length;
+    $("#contador-caracteres").text (quantidadeCaracteres);
+});
