1a21710e9718a5748d4f008bb954dd3621811c7e
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-04-26 02:08:39 -0300

    Organizando [bug ao add novo placar].

--- public/js/main.js
+++ public/js/main.js
@@ -90,55 +90,6 @@ function inicializaMarcadores() {
 
 
 
-// Pontuação do placar.
-function inserePlacar() {
-    var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Seu-nome";
-    var numeroPalavras = $("contador-palavras").text();
-    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>";
-
-    var linha = novaLinhaPlacar(usuario, numeroPalavras);
-    linha.find(".botao-remover").click(removeLinhaPlacar);
-
-    corpoTabela.prepend(linha);
-}
-
-
-
-// Insere uma nova linha dos jogadores.
-function novaLinhaPlacar(usuario, palavras) {
-    var linha = $("<tr>");
-    var colunaUsuario = $("<td>").text(usuario);
-    var colunaPalavras = $("<td>").text(palavras);
-    var colunaRemover = $("<td>");
-
-    var link = $("<a>").attr("href", "#");
-    var icone =$("<i>").addClass("small").addClass("material-icons").text("delete");
-
-    // Icone dentro do <a>
-    link.append(icone);
-
-    // <a> dentro do <td>
-    colunaRemover.append(link);
-
-    // Os três <td>'s dentro do <tr>
-    linha.append(colunaUsuario);
-    linha.append(colunaPalavras);
-    linha.append(colunaRemover);
-
-    return linha;
-}
-
-
-
-// Botão de remover a pontuação do jogador.
-function removeLinhaPlacar (event) {
-    event.preventDefault();
-    $(this).parent().parent().remove();
-}
-
-
-
 // Botão de reiniciar o jogo.
 $("#botao-reiniciar").click (reiniciarJogo);
 
