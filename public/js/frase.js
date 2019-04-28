2cbeb8b3757670343603ee54b5b983048faacf1d
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-04-28 03:33:14 -0300

    Buscando frases no servidor via Ajax pelo id.

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,8 +1,13 @@
 // frase.js
 
 
-// botão para frases aleatórias.
+// Botão para frases aleatórias.
 $("#botao-frase").click(fraseAleatoria);
+
+// Botão para buscar frases via id no servidor.
+$("#botao-busca-id").click(buscaFrase);
+
+
 
 // Frases aleatórias com spinner e mensagem de erro.
 function fraseAleatoria() {
@@ -27,4 +32,32 @@ function trocaFraseAleatoria(data) {
     frase.text(data[numeroAleatorio].texto); // Pega a primeira frase.
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo); //'tempo' pq tá assim no array do servidor.
+}
+
+// Usuário escolhe a frase via ID.
+function buscaFrase() {
+    $("#spinner").toggle();
+
+    var buscaViaId = $("#busca-id").val();
+    var dados = {id: buscaViaId}; // Criacao do objeto JS que guarda a id.
+
+    // Passsando objetos como segundo plano.
+    $.get("http://localhost:3000/frases", dados, trocaFrase).fail(function(){
+        $("#erro").toggle();
+
+        setTimeout(function(){
+            $("#erro").toggle();
+        }, 3000);
+    }).always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+// Troca frase pelo id.
+function trocaFrase(data) {
+    var frase = $(".frase");
+
+    frase.text(data.texto); // 'texto' por causa que tá assim no array do servidor.
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo); // 'tempo' por causa que tá assim no array do servidor.
 }
