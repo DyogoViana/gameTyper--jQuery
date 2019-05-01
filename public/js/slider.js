dfd88e9ad2e4979721e83173632e86605bf4cc9f
Dyogo Viana <dyogo.vianaa@gmail.com>
2019-04-30 23:32:20 -0300

    [Plugin Slick] Carousel jQuery.

---
+++ public/js/slider.js
@@ -0,0 +1,13 @@
+// slider.js
+
+
+// Carousel jQuery -- Slick
+$(function() {
+    $(".slider").slick( {
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
